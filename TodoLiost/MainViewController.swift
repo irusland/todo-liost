a79d3ea16bd3093d1e930ef20f2460b4e9bec835
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2021-11-12 00:24:25 +0500

    rm custom layout

--- TodoLiost/MainViewController.swift
+++ TodoLiost/MainViewController.swift
@@ -126,42 +126,6 @@ class MainViewController: UIViewController {
     }
 }
 
-
-class CustomFlowLayout : UICollectionViewFlowLayout {
-    var insertingIndexPaths = [IndexPath]()
-    
-    override func prepare(forCollectionViewUpdates updateItems: [UICollectionViewUpdateItem]) {
-        super.prepare(forCollectionViewUpdates: updateItems)
-        
-        insertingIndexPaths.removeAll()
-        
-        for update in updateItems {
-            if let indexPath = update.indexPathAfterUpdate,
-               update.updateAction == .insert {
-                insertingIndexPaths.append(indexPath)
-            }
-        }
-    }
-    
-    override func finalizeCollectionViewUpdates() {
-        super.finalizeCollectionViewUpdates()
-        
-        insertingIndexPaths.removeAll()
-    }
-    
-    override func initialLayoutAttributesForAppearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        let attributes = super.initialLayoutAttributesForAppearingItem(at: itemIndexPath)
-        
-        if insertingIndexPaths.contains(itemIndexPath) {
-            attributes?.alpha = 0.0
-            attributes?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-            attributes?.transform = CGAffineTransform(translationX: 0, y: 500.0)
-        }
-        
-        return attributes
-    }
-}
-
 class SquaresViewController: UICollectionViewController {
     var fileCache: FileCache
     var todoItemDetailViewController: TodoItemDetailViewController
@@ -308,8 +272,7 @@ class SmallViewController : SquaresViewController {
     }()
 
     init(with fileCache: FileCache, _ todoItemDetailViewController: TodoItemDetailViewController) {
-        let layout = CustomFlowLayout()
-        
+        let layout = UICollectionViewFlowLayout.init()
         super.init(collectionViewLayout: layout, fileCache, todoItemDetailViewController)
 
         useLayoutToLayoutNavigationTransitions = false
