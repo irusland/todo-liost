e3f1d1fe8bd59401c0e231ee00c96f572d4a101e
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2021-12-20 22:58:16 +0500

    BackendError rename

--- TodoLiost/Main/Net/BackendConnector.swift
+++ TodoLiost/Main/Net/BackendConnector.swift
@@ -17,7 +17,7 @@ class BackendConnector {
     
     private func request<T: Decodable>(
         endpoint: Endpoint,
-        with handler: @escaping (Result<T, BackendErrors>) -> Void,
+        with handler: @escaping (Result<T, BackendError>) -> Void,
         using session: URLSession = .shared
     ) {
         _ = session.request(endpoint) { data, response, error in
@@ -37,29 +37,29 @@ class BackendConnector {
                 handler(Result.success(response))
             } catch {
                 DDLogInfo("Cannot parse \(body) \(error)")
-                handler(Result.failure(BackendErrors.parseError(body, error)))
+                handler(Result.failure(BackendError.parseError(body, error)))
             }
         }
     }
     
-    private func prepareEndpoint(endpointInitialiser: (String) throws -> Endpoint?) -> Result<Endpoint, BackendErrors> {
+    private func prepareEndpoint(endpointInitialiser: (String) throws -> Endpoint?) -> Result<Endpoint, BackendError> {
         guard let token = authViewController.authCredentials?.accessToken else {
-            return Result.failure(BackendErrors.tokenIsNone("Token is none"))
+            return Result.failure(BackendError.tokenIsNone("Token is none"))
         }
         var endpoint: Endpoint?
         do {
             endpoint = try endpointInitialiser(token)
         } catch {
-            return Result.failure(BackendErrors.cannotPrepareEndpoint)
+            return Result.failure(BackendError.cannotPrepareEndpoint)
         }
         guard let endpoint = endpoint else {
-            return Result.failure(BackendErrors.cannotPrepareEndpoint)
+            return Result.failure(BackendError.cannotPrepareEndpoint)
         }
         return Result.success(endpoint)
     }
     
     private func tryRequest<T: Decodable>(
-        handler: @escaping (Result<T, BackendErrors>) -> Void,
+        handler: @escaping (Result<T, BackendError>) -> Void,
         endpoint endpointInitialiser: (String) throws -> Endpoint?,
         using session: URLSession = .shared
     ) {
@@ -73,43 +73,43 @@ class BackendConnector {
         }
     }
 
-    func getList(handler: @escaping (Result<ListModel, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func getList(handler: @escaping (Result<ListModel, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             .list(token: token)
         }
     }
 
-    func merge(with model: MergeModel, handler: @escaping (Result<ListModel, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func merge(with model: MergeModel, handler: @escaping (Result<ListModel, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             try .merge(with: model, token: token)
         }
     }
 
-    func add(todoItem: NewItemModel, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func add(todoItem: NewItemModel, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             try .newItem(with: todoItem, last: lastKnownRevision, token: token)
         }
     }
 
-    func update(at id: UUID, todoItem: NewItemModel, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func update(at id: UUID, todoItem: NewItemModel, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             try .updateItem(with: id, newItemModel: todoItem, last: lastKnownRevision, token: token)
         }
     }
 
-    func remove(by id: UUID, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func remove(by id: UUID, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             try .deleteItem(with: id, last: lastKnownRevision, token: token)
         }
     }
 
-    func get(by id: UUID, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendErrors>) -> Void, using session: URLSession = .shared) {
+    func get(by id: UUID, lastKnownRevision: Int32, handler: @escaping (Result<NewItemResponse, BackendError>) -> Void, using session: URLSession = .shared) {
         tryRequest(handler: handler) { token in
             .item(with: id, last: lastKnownRevision, token: token)
         }
     }
 
-    private func checkStatus(response: URLResponse?) -> BackendErrors? {
+    private func checkStatus(response: URLResponse?) -> BackendError? {
         guard let response = response as? HTTPURLResponse else {
             DDLogError("Response is nil")
             return nil
@@ -118,11 +118,11 @@ class BackendConnector {
         case 200:
             return nil
         case 400:
-            return BackendErrors.unsynchronizedRevision
+            return BackendError.unsynchronizedRevision
         case 404:
-            return BackendErrors.notFound
+            return BackendError.notFound
         case 401:
-            return BackendErrors.unauthorized
+            return BackendError.unauthorized
         default:
             return nil
         }
